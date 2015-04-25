8d0d0b1556a29dc81acc2853b5188e31efa0f274
Derek Webb <dwebb.af@gmail.com>
2015-04-24 22:41:25 -0700

    Added icon file to target.

--- Rise_of_Music_Solution/package.bat
+++ Rise_of_Music_Solution/package.bat
@@ -14,6 +14,7 @@ mkdir Rise_of_Music_target\scenario\Scripts\Rise_of_Music
 copy Rise_of_Music\bin\Release\CSCore.dll Rise_of_Music_target\scenario\Scripts\Rise_of_Music\CSCore.dll
 copy Rise_of_Music\bin\Release\CSCore.xml Rise_of_Music_target\scenario\Scripts\Rise_of_Music\CSCore.xml
 copy "Rise_of_Music\bin\Release\Rise of Music.exe" "Rise_of_Music_target\scenario\Scripts\Rise_of_Music\Rise of Music.exe"
+copy Rise_of_Music\rise-of-music.ico Rise_of_Music_target\scenario\Scripts\Rise_of_Music\rise-of-music.ico
 copy ..\LICENSE Rise_of_Music_target\scenario\Scripts\Rise_of_Music\LICENSE
 copy ..\README.md Rise_of_Music_target\scenario\Scripts\Rise_of_Music\README.md
 copy ..\Rise_of_Music.bhs Rise_of_Music_target\scenario\Scripts\Rise_of_Music\Rise_of_Music.bhs
